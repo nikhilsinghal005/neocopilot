c0b7ede420c2cc08ca60da00eebbeec0432111e8
WarpDrive\VidyuthDataLabs <nikhilsinghal005@gmail.com>
2025-01-05 13:25:55 +0530

    adding a new tab and updating package.json

---
+++ webview-chat/src/pages/Coworker.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Coworker: React.FC = () => {
+  return (
+    <div>
+      <h1>Coworker</h1>
+      <p>This is the coworker page content.</p>
+    </div>
+  );
+};
+
+export default Coworker;
